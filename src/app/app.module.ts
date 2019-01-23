546016e1b358bdd7f0b0d6bca2b333e3ba8dfa92
Jose Eduardo Gonzalez <jogonzalez@mides.interno>
2019-01-23 16:33:39 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchComponent } from "./components/search/search.component";
+import { ArtistaComponent } from "./components/artista/artista.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { ROUTES } from "./app.routes";
 
-import { AppComponent } from './app.component';
+//importar rutas
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
